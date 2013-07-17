831e08947182a89201af5df29ce5ec5c058ac312
flashmath <flash_math@yahoo.fr>
2013-07-17 17:42:45 +0200

    version 0.0.9.2

--- admin/views/directmails/tmpl/default.php
+++ admin/views/directmails/tmpl/default.php
@@ -48,9 +48,28 @@ if (!empty( $this->sidebar)) : ?>
 	<div class="filter-search btn-group pull-left">
 				<label for="filter_search" class="element-invisible"><?php echo JText::_('COM_DIRECTMAIL_SEARCH_IN_TITLE');?></label>
 				<input type="text" name="filter_search" id="filter_search" placeholder="<?php echo JText::_('COM_DIRECTMAIL_SEARCH_IN_TITLE'); ?>" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_DIRECTMAIL_SEARCH_IN_TITLE'); ?>" />
-			</div>
+	</div>
+    <div class="btn-group pull-left">
+				<button type="submit" class="btn hasTooltip" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>"><i class="icon-search"></i></button>
+				<button type="button" class="btn hasTooltip" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>" onclick="document.id('filter_search').value='';this.form.submit();"><i class="icon-remove"></i></button>
+	</div>
+    
+    <div class="btn-group pull-right hidden-phone">
+				<label for="limit" class="element-invisible"><?php echo JText::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC');?></label>
+				<?php echo $this->pagination->getLimitBox(); ?>
+	</div>
+    
+    <div class="btn-group pull-right hidden-phone">
+				<label for="directionTable" class="element-invisible"><?php echo JText::_('JFIELD_ORDERING_DESC');?></label>
+				<select name="directionTable" id="directionTable" class="input-medium" onchange="Joomla.orderTable()">
+					<option value=""><?php echo JText::_('JFIELD_ORDERING_DESC');?></option>
+					<option value="asc" <?php if ($listDirn == 'asc') echo 'selected="selected"'; ?>><?php echo JText::_('JGLOBAL_ORDER_ASCENDING');?></option>
+					<option value="desc" <?php if ($listDirn == 'desc') echo 'selected="selected"'; ?>><?php echo JText::_('JGLOBAL_ORDER_DESCENDING');?></option>
+				</select>
+	</div>        
 </div>
 <div class="clearfix"></div>
+
 <table class="table table-striped" id="articleList">
 	<thead>
       	<tr>
