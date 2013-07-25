6349d5390e21e0015ed358c26976b9606236edf0
flashmath <g25E9777563ekf>
2013-07-25 18:40:58 +0200

    ajout d'un controlleur test

---
+++ site/controllers/test.php
@@ -0,0 +1,12 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die('');
+ 
+ 
+/**
+ * Directmail Component Controller
+ */
+class DirectMailControllerTest extends JControllerLegacy
+{
+}
+?>
