52efe547dbac090b5d8a55b08cfbf6cd6d234017
flashmath <flash_math@yahoo.fr>
2013-07-29 14:27:48 +0200

    nom du repertoire

--- site/models/formulaire.php
+++ site/models/formulaire.php
@@ -39,4 +39,54 @@ class DirectmailModelFormulaire extends JModelForm
 		// Load the parameters.
 		$this->setState('params', $params);
 	}
+	
+	public function getData()
+	{
+		if ($this->data === null)
+		{
+			$this->data=new stdClass;
+			
+			$app = JFactory::getApplication();
+			$params = JComponentHelper::getParams('com_users');
+			
+			// Override the base user data with any data in the session.
+			$temp = (array) $app->getUserState('com_directmail.formulaire.data', array());
+			foreach ($temp as $k => $v)
+			{
+				$this->data->$k = $v;
+			}
+			
+			$dispatcher = JEventDispatcher::getInstance();
+			$results = $dispatcher->trigger('onContentPrepareData', array('com_directmail.formulaire', $this->data));
+			
+			// Check for errors encountered while preparing the data.
+			if (count($results) && in_array(false, $results, true))
+			{
+				$this->setError($dispatcher->getError());
+				$this->data = false;
+			}
+		}
+		
+		return $this->data;
+	}
+	
+	public function submit($temp)
+	{
+		$config = JFactory::getConfig();
+		$db = $this->getDbo();
+		
+		$data = (array) $this->getData();
+		
+		// Merge in the registration data.
+		foreach ($temp as $k => $v)
+		{
+			$data[$k] = $v;
+		}
+		
+		$data['fromname'] = $config->get('fromname');
+		$data['mailfrom'] = $config->get('mailfrom');
+		$data['sitename'] = $config->get('sitename');
+		$data['siteurl'] = JUri::root();
+	
+	}
 }
